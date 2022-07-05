4d0745383e4a0a64c8f97443d87250dc417a2043
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-07-05 12:00:46 +0100

    Add categories section

--- src/components/home.js
+++ src/components/home.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { GiWorld } from 'react-icons/gi';
 
 const Home = () => (
-  <section className="home-banner">
-    <span className="world-map"><GiWorld /></span>
-    <div className="banner-text">
-      <p className="banner-title">world statistics</p>
-      <span className="case-title">cases:12243647454 </span>
-      {' '}
-      <br />
-      <span className="case-title">deaths: 12243647454</span>
-      <br />
-      <span className="case-title">recovered:12243647454</span>
-      <br />
+  <section className="body">
+    <div className="home-banner">
+      <span className="world-map"><GiWorld /></span>
+      <div className="banner-text">
+        <p className="banner-title">world statistics</p>
+        <span className="case-title">cases:12243647454 </span>
+        {' '}
+        <br />
+        <span className="case-title">deaths: 12243647454</span>
+        <br />
+        <span className="case-title">recovered:12243647454</span>
+        <br />
+      </div>
+    </div>
+    <div className="categories-container">
+      <p className="categories">Africa</p>
+      <p>Asia</p>
+      <p>Europe</p>
+      <p>America</p>
     </div>
   </section>
 );
