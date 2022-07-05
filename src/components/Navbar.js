4c1326699755b5454bfdee301d9eeb1fe074304d
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-07-05 10:32:31 +0100

    Add Css to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { FaMicrophone, FaArrowLeft } from 'react-icons/fa';
 
 const Navbar = () => (
   <header>
-    <span><FaArrowLeft /></span>
+    <span className="arrow-class"><FaArrowLeft /></span>
     <p className="title">Covid-19 Tracker</p>
     <div className="icons">
       <span className="microphone"><FaMicrophone /></span>
