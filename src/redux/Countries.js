f5b50b1d652eb05f99d5b5962c4ef3c4558db248
nedjwab <62156607+nedjwab@users.noreply.github.com>
2022-07-05 17:51:26 +0100

    Install Redux-thunk

--- src/redux/Countries.js
+++ src/redux/Countries.js
@@ -4,39 +4,39 @@ const GET_COUNTRIES = 'Covid19-tracker/GET_COUNTRIES';
 const url = 'https://disease.sh/v3/covid-19/countries';
 
 export const getCountries = (payload) => ({
-    type: GET_COUNTRIES,
-    payload,
-  });
+  type: GET_COUNTRIES,
+  payload,
+});
 
-  export const fetchCity = () => async (dispatch) => {
-    const response = await axios.get(url);
-    const data = await response.data;
-    const countries = [];
-    try {
-      data.forEach((el) => {
-        countries.push({
-          id:el.countryInfo.id,
-          country: el.country,
-          flags: el.countryInfo.flag,
-          cases:el.cases,
-          deaths:el.deaths,
-          recovered:el.recovered,
-          continent:el.continent,
-          tests:el.tests,
-          iso:el.countryInfo.iso2
-        });
+export const fetchCity = () => async (dispatch) => {
+  const response = await axios.get(url);
+  const data = await response.data;
+  const countries = [];
+  try {
+    data.forEach((el) => {
+      countries.push({
+        id: el.countryInfo.id,
+        country: el.country,
+        flags: el.countryInfo.flag,
+        cases: el.cases,
+        deaths: el.deaths,
+        recovered: el.recovered,
+        continent: el.continent,
+        tests: el.tests,
+        iso: el.countryInfo.iso2,
       });
-      dispatch(getCountries(countries));
-    } catch (error) {
-      <h2>{error}</h2>;
-    }
-  };
-
-  export default function countryReducer(state = [], action) {
-    switch (action.type) {
-      case GET_COUNTRIES:
-        return action.payload;
-      default:
-        return state;
-    }
+    });
+    dispatch(getCountries(countries));
+  } catch (error) {
+    <h2>{error}</h2>;
+  }
 };
+
+export default function countryReducer(state = [], action) {
+  switch (action.type) {
+    case GET_COUNTRIES:
+      return action.payload;
+    default:
+      return state;
+  }
+}
